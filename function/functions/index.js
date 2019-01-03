7f879639ae055e460289e2a4d9e62a0812a71f82
IzumiSy <beetle-noise@gmx.com>
2019-01-04 01:04:23 +0900

    Implemented title fetching function

--- function/functions/index.js
+++ function/functions/index.js
@@ -1,7 +1,38 @@
+const HttpStatus = require('http-status-code')
+const { JSDOM } = require('jsdom')
 const functions = require('firebase-functions');
+const urlValidator = require('valid-url')
+const request = require('request')
+const cors = require('cors')
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// TODO: CORS設定はちゃんとやる
+exports.fetchTitle = functions.https.onRequest((req, res) => {
+  cors({ origin: true })(req, res, () => {
+    const targetUrl = req.query.url
+
+    console.info('Fetch: ', req.query.url)
+
+    if (!targetUrl || !urlValidator.isUri(targetUrl)) {
+      res.status(HttpStatus.BAD_REQUEST).end()
+    }
+
+    request(targetUrl, (e, _, body) => {
+      if (e) {
+        console.error(e)
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).end()
+      }
+
+      try {
+        const dom = new JSDOM(body)
+        const $title = dom.window.document.querySelector('title')
+        if ($title) {
+          res.send($title.textContent).end()
+        }
+        res.send('')
+      } catch (err) {
+        console.error(err)
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).end()
+      }
+    })
+  })
 });
