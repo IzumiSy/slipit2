b7c85540015d061d62aa8d3729f9ae525e22c6d4
IzumiSy <beetle-noise@gmx.com>
2019-09-17 22:29:59 +0900

    Allows CORS only on development

--- functions/fetchTitle.js
+++ functions/fetchTitle.js
@@ -3,23 +3,34 @@ const urlValidator = require('valid-url')
 const request = require('request')
 const HtmlParser = require('node-html-parser')
 
+withCorsAllowedOnDev = (src) => {
+  // Allows CORS only for development environment
+
+  const aca =
+    process.env.NODE_ENV != "production" ? {
+      headers: { "Access-Control-Allow-Origin" : "*"}
+    } : {}
+
+  return Object.assign(src, aca);
+}
+
 exports.handler = (event, context, callback) => {
   const targetUrl = event.queryStringParameters.url
 
   if (!targetUrl || !urlValidator.isUri(targetUrl)) {
-    callback(null, { 
+    callback(null, withCorsAllowedOnDev({ 
       statusCode: HttpStatus.BAD_REQUEST, 
       body: "Must be a valid URL" 
-    })
+    }))
     return
   }
 
   request(targetUrl, (e, _, body) => {
     if (e) {
-      callback(null, { 
+      callback(null, withCorsAllowedOnDev({ 
         statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
         body: "Internal error"
-      })
+      }))
       return
     }
 
@@ -37,15 +48,15 @@ exports.handler = (event, context, callback) => {
       if ($title) result.title = $title.text
       if ($description.length) result.description = $description[0].content
 
-      callback(null, { 
+      callback(null, withCorsAllowedOnDev({ 
         statusCode: HttpStatus.OK, 
         body: JSON.stringify(result) 
-      })
+      }))
     } catch (err) {
-      callback(null, { 
+      callback(null, withCorsAllowedOnDev({ 
         statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
         body: "Internal error"
-      })
+      }))
     }
   })
 }
