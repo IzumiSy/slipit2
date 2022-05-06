6623b639ebce596c0098db1936d791eccec6ed87
Seiya IZUMI <IzumiSy@users.noreply.github.com>
2022-05-06 18:43:52 +0900

    Initialize functions (#39)
    * npx firebase init function
    
    * Removed code commented out
    
    * Now functions will be served on start command
    
    * Updated function name
    
    * Split install step and build step
    
    * Fix lint errors

---
+++ functions/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    quotes: ["error", "double"],
+    indent: ["error", 2],
+  },
+};
